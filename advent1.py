760add09afe364f1852041acd0b293c1a1f2471a
Roxanne Seeley <a02343790@usu.edu>
2023-12-01 12:55:50 -0700

    Starting day 1

---
+++ advent1.py
@@ -0,0 +1,20 @@
+
+def main():
+    #go line by line through file, split into characters, add to a list
+    #then find first integer by looping through list from front
+    #loop through list from end to find last int
+    #if there's only one int it's counted twice
+    #stick the strings together then convert to int
+    #add to list
+
+    #once we've looped through all the lines, add each int in the list and return
+
+    calibration = open(calibrate.txt)
+    for line in calibration:
+
+
+
+
+
+
+main()
