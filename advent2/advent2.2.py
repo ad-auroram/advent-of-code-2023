af8ab7f17d4a50cdbfb86acee60b392076b20ce6
Rox <roxseeley@gmail.com>
2023-12-03 16:53:45 -0700

    getting some ideas down.

---
+++ advent2/advent2.2.py
@@ -0,0 +1,35 @@
+def findProduct(line):
+    red = 0
+    green = 0
+    blue = 0
+    cases = line.split(",")
+    for case in cases:
+        case = case.split(";")
+        for i in case:
+            i = i.strip()
+            i = i.split(" ")
+            if "red" == i[1]:
+                num = int(i[0])
+                if num > red:
+                    red = num
+            if "green" == i[1]:
+                num = int(i[0])
+                if num > green:
+                    green = num
+            if "blue" == i[1]:
+                num = int(i[0])
+                if num > blue:
+                    blue = num
+    return True
+def main():
+    games = open("input-day2.txt")
+    total = 0
+    for line in games:
+        line = line.split(":")
+        product = findProduct(line[1])
+        total += product
+
+    print(total)
+
+
+main()
